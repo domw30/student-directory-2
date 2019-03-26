68dae29ff6bac90b2b9b9bdc0e1c398544ee9e29
Dominic White <your email>
2019-03-26 08:26:17 +0000

    Add interactive menu

--- directory.rb
+++ directory.rb
@@ -25,6 +25,28 @@ def input_students
   students
 end
 
+def interactive_menu
+  students = []
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    selection = gets.chomp
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
+
 
 def print_header
   puts "The students of Villains Academy"
@@ -40,6 +62,8 @@ end
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
+
+interactive_menu
 
 students = input_students
 print_header
